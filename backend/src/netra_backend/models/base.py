0ff21052c8da40efda68c281119ffb24842c1d0f
Subhadip-Paul2006 <subhadippaulff@gmail.com>
2026-08-15 14:31:55 +0530

    feat: add declarative base model

---
+++ backend/src/netra_backend/models/base.py
@@ -0,0 +1,9 @@
+"""Pure SQLAlchemy 2.x Base Declarative Model class."""
+
+from sqlalchemy.orm import DeclarativeBase
+
+
+class Base(DeclarativeBase):
+    """Base class for all SQLAlchemy 2.x ORM models."""
+
+    pass
